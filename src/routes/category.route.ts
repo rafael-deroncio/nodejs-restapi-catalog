8dd2db7cb7007ff3c1a3f7725148923f346658cd
rafael-deroncio <rafael.deroncio@hotmail.com>
2024-04-16 00:17:53 -0300

    apply middlewares

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import controller from "../controllers/category.controller";
+import middleware from "../middlewares/middlewares";
 
 const category: Router = Router();
 
-category.get('catalog/categories', controller.category.paged);
-category.post('catalog/category', controller.category.post);
+category.get('catalog/categories', middleware.pagination, controller.category.paged);
+category.post('catalog/category', middleware.category, controller.category.post);
 category.get('catalog/category/:id', controller.category.get);
-category.put('catalog/category/:id', controller.category.put);
+category.put('catalog/category/:id', middleware.category, controller.category.put);
 category.delete('catalog/category/:id', controller.category.delete);
 
 export default category;
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import controller from "../controllers/product.controller";
+import middleware from "../middlewares/middlewares";
 
 const product: Router = Router();
 
-product.get('catalog/products', controller.product.paged);
-product.post('catalog/product', controller.product.post);
+product.get('catalog/products', middleware.pagination, controller.product.paged);
+product.post('catalog/product', middleware.product, controller.product.post);
 product.get('catalog/product/:id', controller.product.get);
-product.put('catalog/product/:id', controller.product.put);
+product.put('catalog/product/:id', middleware.product, controller.product.put);
 product.delete('catalog/product/:id', controller.product.delete);
 
 export default product;
