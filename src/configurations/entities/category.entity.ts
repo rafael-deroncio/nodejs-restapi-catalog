92eda2427c10b27802bfd7f9c5c7bb25a5e11833
rafael-deroncio <rafael.deroncio@hotmail.com>
2024-04-16 16:54:00 -0300

    update entities

--- src/configurations/entities/category.entity.ts
+++ src/configurations/entities/category.entity.ts
@@ -1,26 +1,35 @@
 import { BaseEntity, Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { ProductEntity } from "./product.entity";
+import { Expose } from "class-transformer";
 
 @Entity({ name: 'categories'})
 export class CategoryEntity extends BaseEntity {
+    
+    @Expose()
     @PrimaryGeneratedColumn('increment')
     id!: number;
 
+    @Expose()
     @Column({ type: 'varchar' })
     name!: string;
 
+    @Expose()
     @Column({ type: 'varchar' })
     description!: string;
 
+    @Expose()
     @Column({ type: 'boolean', default: true })
     active!: boolean;
 
-    @OneToMany(() => ProductEntity, product => product.category)
+    @Expose()
+    @OneToMany(() => ProductEntity, product => product.category, {eager: true, cascade: true})
     products: ProductEntity | undefined;
 
+    @Expose()
     @CreateDateColumn()
     created!: Date;
 
+    @Expose()
     @UpdateDateColumn()
     updated!: Date;
 }
--- src/configurations/entities/product.entity.ts
+++ src/configurations/entities/product.entity.ts
@@ -1,32 +1,44 @@
-import { BaseEntity, Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { CategoryEntity } from "./category.entity";
+import { Expose } from "class-transformer";
 
 @Entity({ name: 'products' })
 export class ProductEntity extends BaseEntity {
+    
+    @Expose()
     @PrimaryGeneratedColumn()
     id!: number;
 
+    @Expose()
     @Column({ type: 'varchar' })
     name!: string;
 
+    @Expose()
     @Column({ type: 'varchar' })
     description!: string;
 
+    @Expose()
     @Column('decimal', { precision: 10, scale: 2 })
     price!: number;
 
+    @Expose()
     @Column({ type: 'int' })
     stock!: number;
 
+    @Expose()
     @Column({ type: 'boolean', default: true })
     active!: boolean;
 
+    @Expose()
+    @JoinColumn({ name: 'category' })
     @ManyToOne(() => CategoryEntity, category => category.products)
     category!: CategoryEntity;
 
+    @Expose()
     @CreateDateColumn()
     created!: Date;
 
+    @Expose()
     @UpdateDateColumn()
     updated!: Date;
 }
