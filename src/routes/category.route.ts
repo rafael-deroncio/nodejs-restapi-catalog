d1c975078a7a7835d3d466df5382996c2e81245a
rafael-deroncio <rafael.deroncio@hotmail.com>
2024-04-18 08:07:55 -0300

    update routes

--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -4,10 +4,10 @@ import middleware from "../middlewares/middlewares";
 
 const category: Router = Router();
 
-category.get('catalog/categories', middleware.pagination, controller.category.paged);
-category.post('catalog/category', middleware.category, controller.category.post);
-category.get('catalog/category/:id', controller.category.get);
-category.put('catalog/category/:id', middleware.category, controller.category.put);
-category.delete('catalog/category/:id', controller.category.delete);
+category.get('/catalog/categories', middleware.pagination, controller.category.paged);
+category.post('/catalog/category', middleware.category, controller.category.post);
+category.get('/catalog/category/:id', controller.category.get);
+category.put('/catalog/category/:id', middleware.category, controller.category.put);
+category.delete('/catalog/category/:id', controller.category.delete);
 
 export default category;
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -4,10 +4,10 @@ import middleware from "../middlewares/middlewares";
 
 const product: Router = Router();
 
-product.get('catalog/products', middleware.pagination, controller.product.paged);
-product.post('catalog/product', middleware.product, controller.product.post);
-product.get('catalog/product/:id', controller.product.get);
-product.put('catalog/product/:id', middleware.product, controller.product.put);
-product.delete('catalog/product/:id', controller.product.delete);
+product.get('/catalog/products', middleware.pagination, controller.product.paged);
+product.post('/catalog/product', middleware.product, controller.product.post);
+product.get('/catalog/product/:id', controller.product.get);
+product.put('/catalog/product/:id', middleware.product, controller.product.put);
+product.delete('/catalog/product/:id', controller.product.delete);
 
 export default product;
